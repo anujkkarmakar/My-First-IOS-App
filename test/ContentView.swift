b3e8931ca4af98a080027a0889efde1e6009223c
Anuj Kumar Karmakar <anujkarmakar999@gmail.com>
2023-10-17 10:28:27 +0530

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Anuj Kumar Karmakar on 16/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("This is my first App")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Anuj Kumar Karmakar on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
